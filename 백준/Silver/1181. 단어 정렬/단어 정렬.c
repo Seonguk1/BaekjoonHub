0df9fcd745bdec9007597005b6350f745ddf4507
seonguk <dnrdl1109@gmail.com>
2025-07-05 14:18:23 +0900

    [Silver V] Title: 단어 정렬, Time: 24 ms, Memory: 2308 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int compare(const void* a, const void* b){
+	a = (const char*)a;
+	b = (const char*)b;
+	if(strlen(a) == strlen(b)){
+		return strcmp(a,b);
+	}else{
+		return strlen(a) - strlen(b);
+	}
+}
+
+int main(){
+	int n;
+	
+	scanf("%d", &n);
+	char arr[n][51];
+	for(int i=0;i<n;i++){
+		scanf("%s", arr[i]);
+	}
+	
+	qsort(arr, n, 51, compare);
+	
+	for(int i=0;i<n;i++){
+        if(strcmp(arr[i],arr[i-1]) == 0) continue;
+		printf("%s\n", arr[i]);
+	}
+	
+	return 0;
+}
