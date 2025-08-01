b3edf0c22a6dc9fda0c975e2949f9f8f5c9e4035
seonguk <dnrdl1109@gmail.com>
2025-08-02 05:29:33 +0900

    [Silver II] Title: 나무 자르기, Time: 204 ms, Memory: 4900 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.c
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.c
@@ -1,34 +1,26 @@
 #include <stdio.h>
 
-int main() {
-    int n, m;
+int main(){
+    int n,m;
     scanf("%d %d", &n, &m);
     int arr[1000001], max = 0;
-
-    for (int i = 0; i < n; i++) {
+    for(int i=0;i<n;i++){
         scanf("%d", &arr[i]);
-        if (arr[i] > max) max = arr[i];
+        if(arr[i] > max) max = arr[i];
     }
-
-    long long left = 0, right = max, answer = 0;
-
-    while (left <= right) {
-        long long mid = (left + right) / 2;
+    long long left = 0, right = max, mid, answer;
+    while(left <= right){
+        mid = (left + right) / 2;
         long long sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (arr[i] > mid)
+        for(int i=0;i<n;i++){
+            if(arr[i] > mid)
                 sum += arr[i] - mid;
         }
-
-        if (sum >= m) {
-            answer = mid;       // 조건을 만족했으므로 저장
-            left = mid + 1;     // 더 높게 자를 수 있는지 시도
-        } else {
-            right = mid - 1;    // 너무 많이 잘라서 부족함 → 낮춰야 함
-        }
+        if(sum >= m){
+            answer = mid;
+            left = mid+1;
+        } 
+        else right = mid-1;
     }
-
     printf("%lld", answer);
-    return 0;
 }
