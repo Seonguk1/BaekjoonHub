83dc31d93c2a4ecd347f980ddb5a133e43f1e8fc
seonguk <dnrdl1109@gmail.com>
2025-11-04 15:45:21 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 120 ms, Memory: 21620 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.c
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
-char str[200001], a;
-int q,l,r, dp[200001];
+char str[200001], a, prev;
+int q,l,r, dp['z'-'a'+1][200001];
 int main(){
     scanf("%s %d ", str, &q);
-    dp[0]=0;
+    
+    for(int i=0;i<'z'-'a'+1;i++){
+        dp[i][0]=0;
+        for(int j=1;str[j-1];j++){   
+            dp[i][j] = dp[i][j-1] + (str[j-1] == 'a'+i ? 1 : 0);
+        }
+    }
+    
     for(int i=0;i<q;i++){
         scanf("%c %d %d ",&a,&l,&r);
-        for(int i=1;str[i-1];i++){   
-            dp[i] = dp[i-1] + (str[i-1]==a ? 1 : 0);
-        }
-        printf("%d\n", dp[r+1] - dp[l]);
+        printf("%d\n", dp[a-'a'][r+1] - dp[a-'a'][l]);
     }
     
     return 0;
